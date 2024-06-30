b1590a55eb5b7a4a5b6451f2b6347d47012f240c
babakfp <babak.bxf@gmail.com>
2024-06-30 16:22:59 +0330

    Escaped "+" in open and close fence

--- src/frontmatterRegex.ts
+++ src/frontmatterRegex.ts
@@ -4,6 +4,9 @@ export const frontmatterRegex = (
     open: OptionsOutput["fence"]["open"],
     close: OptionsOutput["fence"]["close"]
 ) => {
+    open.replaceAll("+", "\\+")
+    close.replaceAll("+", "\\+")
+
     return new RegExp(
         `^${open}(?:\r?\n|\r)(?:([\\s\\S]*?)(?:\r?\n|\r))?${close}(?:\r?\n|\r|$)`
     )
